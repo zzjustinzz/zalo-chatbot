c210f8d0faa5b0fdd4224fcbc7d4e55e509f0eec
Thanh Tran <thanhtdfu@gmail.com>
2017-12-16 19:58:40 +0700

    update config

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+    environment: 'production',
     db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
     socketPort: 5100,
     /**
